e49be7f7f8edc5e6dfeae1c8cb74db96c1d82fb0
johi98 <hgfdsa554@gmail.com>
2021-03-20 21:28:47 +0900

    Edit | Obstacle prefab

--- Assets/02. PJH/1.Scripts/CubeInit.cs
+++ Assets/02. PJH/1.Scripts/CubeInit.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class CubeInit : MonoBehaviour
 {
     public List<GameObject> obstacleList;
-    public GameObject thisCube;
-    public GameObject initCube;
  
     bool isInit;
     int initCubeNum;
@@ -20,7 +18,7 @@ public class CubeInit : MonoBehaviour
 
     public void ObstacleCreate()
     {
-        float cubeHeight = thisCube.GetComponent<MeshRenderer>().bounds.size.y;
+        float cubeHeight = gameObject.GetComponent<MeshRenderer>().bounds.size.y;
 
         float xRange = Random.Range(-8, 8); //생성 범위
         float zRange = Random.Range(7, 12);
@@ -28,6 +26,8 @@ public class CubeInit : MonoBehaviour
 
         GameManager.nextInitHeight += cubeHeight;
         // Debug.Log(GameManager.nextInitHeight);
+
+        initCubeNum = Random.Range(0, obstacleList.Count);
 
         Instantiate(obstacleList[initCubeNum], new Vector3(xRange, GameManager.nextInitHeight + 0.1f, zRange),
             Quaternion.Euler(Quaternion.identity.x, Random.Range(0, 360), Quaternion.identity.z));
