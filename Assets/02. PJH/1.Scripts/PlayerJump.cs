673338aea6ef24a79793d69f19d04bbce726d9fa
johi98 <hgfdsa554@gmail.com>
2021-04-11 21:21:34 +0900

    Edit | Error Fix 0.02

--- Assets/02. PJH/1.Scripts/PlayerJump.cs
+++ Assets/02. PJH/1.Scripts/PlayerJump.cs
@@ -69,11 +69,10 @@ public class PlayerJump : MonoBehaviour//, IPointerDownHandler, IPointerUpHandle
         {
 
             soundSet.JumpSoundChange();
-			//Debug.Log("왜소리안나와");
+
 			rig.AddForce(Vector3.up * jumpPower * Time.deltaTime, ForceMode.Impulse);
 			MyAnimator.JumpAnimaion();
-            //GameManager.reAnimationNum = Random.Range(0, 7);
-            //Debug.Log("reAni: " + GameManager.reAnimationNum);
+            Debug.Log("점프함");
             isjump = false;
             jumpT = false;
             
